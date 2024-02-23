219089d4dd6dfeb165418dadc9cd97b09a0a86a8
Wyrdo Kward <wyrdokward@gmail.com>
2024-02-23 18:23:26 +0100

    Add Resource type

---
+++ Assets/_Scripts/Cards/Logic/ResourceLogic.cs
@@ -0,0 +1,17 @@
+﻿using Assets._Scripts.Utilities.Enums;
+using System;
+
+namespace Assets._Scripts.Cards.Logic
+{
+    internal class ResourceLogic : CardLogic
+    {
+        public override ECardType CardType => ECardType.Resource;
+
+        internal override Action GetReceipe()
+        {
+            if (!base.VerifyReceipe()) return null;
+
+            return null;
+        }
+    }
+}
---
+++ Assets/_Scripts/ScriptableObjects/ResourceCardSO.cs
@@ -0,0 +1,18 @@
+﻿using Assets._Scripts.Cards.Logic;
+using UnityEngine;
+
+namespace Assets._Scripts.ScriptableObjects
+{
+    [ExecuteInEditMode]
+    [CreateAssetMenu(fileName = "New Resource", menuName = "Card/Resource")]
+
+    internal class ResourceCardSO : BaseCardSO
+    {
+        public override void InitializedCardWithScriptableObject(GameObject cardBodyGO)
+        {
+            base.InitializedCardWithScriptableObject(cardBodyGO);
+
+            cardBodyGO.AddComponent<ResourceLogic>();
+        }
+    }
+}
